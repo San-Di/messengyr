d3ee1430b7d6aa7fdf4c1d949b36224a65f5054f
Saw Thinkar <sawthinkarnayhtoo@gmail.com>
2018-05-09 11:14:38 +0630

    Using API authentication in Guardian

--- lib/messengyr_web/controllers/api/room_controller.ex
+++ lib/messengyr_web/controllers/api/room_controller.ex
@@ -1,6 +1,9 @@
 defmodule MessengyrWeb.RoomController do
     use MessengyrWeb, :controller
     alias Messengyr.Chat
+    alias MessengyrWeb.ErrorView
+
+    plug Guardian.Plug.EnsureAuthenticated, error_handler: __MODULE__
 
     def index(conn, _params) do
       user = Guardian.Plug.current_resource(conn)
@@ -10,6 +13,12 @@ defmodule MessengyrWeb.RoomController do
         rooms: rooms,
         me: user,
       })
+    end
+
+    def auth_error(conn, {_type, _reason}, _opts) do
+      conn
+      |> put_status(401)
+      |> render(ErrorView, "error.json", message: "You are not authenticated.")
     end
 
 end
